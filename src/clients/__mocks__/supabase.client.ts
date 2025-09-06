22c6d6e7dcf4453109bd4b0dd81afde49efbb711
SzymiYay <szymoon09@gmail.com>
2025-09-06 22:01:26 +0200

    fix: supabase mock

---
+++ src/clients/__mocks__/supabase.client.ts
@@ -0,0 +1,9 @@
+export const supabase = {
+  from: jest.fn(() => ({
+    select: jest.fn().mockReturnThis(),
+    insert: jest.fn().mockReturnThis(),
+    update: jest.fn().mockReturnThis(),
+    delete: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockResolvedValue({ data: [], error: null })
+  }))
+}
